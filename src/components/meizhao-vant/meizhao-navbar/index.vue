7110db90a8ff722acae0be636b37fd202e49fdd5
向浮钢 <1140674759@qq.com>
2020-11-27 18:17:15 +0800

    二次封装vant 的vanbar

---
+++ src/components/meizhao-vant/meizhao-navbar/index.vue
@@ -0,0 +1,79 @@
+<!--name-->
+<template>
+  <div class="meizhao-vanbar">
+    <van-nav-bar
+      :left-text="meizhaoVanbar.leftText"
+      :right-text="meizhaoVanbar.rightText"
+      :left-arrow="meizhaoVanbar.leftArrow"
+      :border="meizhaoVanbar.border"
+      :fixed="meizhaoVanbar.fixed"
+      :placeholder="meizhaoVanbar.placeholder"
+      :z-index="meizhaoVanbar.zIndex"
+      :safe-area-inset-top="meizhaoVanbar.safeAreaInsetTop"
+      @click-left="clickLeft"
+      @click-right="clickRight"
+    >
+      <template #title>
+        <div>{{ meizhaoVanbar.title }}</div>
+      </template>
+      <template #left>
+        <van-icon v-if="meizhaoVanbar.leftIcon" :name="meizhaoVanbar.leftName" :size="meizhaoVanbar.leftSize" />
+        <img v-if="meizhaoVanbar.leftImg" :src="meizhaoVanbar.leftimg">
+      </template>
+      <template #right>
+        <van-icon v-if="meizhaoVanbar.rightIcon" :name="meizhaoVanbar.rightName" :size="meizhaoVanbar.rightSize" />
+        <img v-if="meizhaoVanbar.rightImg" :src="meizhaoVanbar.rightimg">
+      </template>
+    </van-nav-bar>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  components: {},
+
+  props: {
+    meizhaoVanbar: { type: Object, default: () => { } }
+  },
+
+  data() {
+    return {
+
+    }
+  },
+  // 监听属性,类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  methods: {
+    clickLeft(value) {
+      this.$emit('clickLeft')
+    },
+    clickRight(value) {
+      this.$emit('clickRight')
+    }
+  },
+  // 生命周期-创建之前
+  beforeCreate() { },
+  // 生命周期-创建完成（可以访问当前this实例）
+  created() { },
+  // 生命周期-挂载之前
+  beforeMount() { },
+  // 生命周期-挂载完成（可以访问DOM元素）
+  mounted() { },
+  // 生命周期-更新之前
+  beforeUpdate() { },
+  // 生命周期-更新之后
+  updated() { },
+  // 生命周期-销毁之前
+  beforeDestroy() { },
+  // 生命周期-销毁完成
+  destroyed() { },
+  // 如果页面有keep-alive缓存功能，这个函数会触发
+  activated() { }
+}
+</script>
+<style scoped lang='scss'>
+</style>
--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -1,15 +1,16 @@
 <!--测试-->
 <template>
   <div class="Test">
-    <div class="shell"></div>
+    <!-- <div class="shell"></div> -->
+    <meizhaoVant @clickLeft="clickLeft" @clickRight="clickRight" :meizhaoVanbar="meizhaoVanbar"></meizhaoVant>
   </div>
 </template>
 
 <script>
-
+import meizhaoVant from 'components/meizhao-vant/meizhao-navbar/index'
 export default {
 
-  components: {},
+  components: { meizhaoVant },
 
   props: {},
 
@@ -19,10 +20,31 @@ export default {
     }
   },
   // 监听属性,类似于data概念
-  computed: {},
+  computed: {
+    meizhaoVanbar() {
+      const aaa = {
+        title: '没找',
+        // leftText: '返回',
+        // rightText: '前进',
+        leftArrow: true,
+        RIGHT: true,
+        rightName: 'search',
+        leftImg: true,
+        leftimg: require('../../assets/imges/shop/banner.png')
+      }
+      return aaa
+    }
+  },
   // 监控data中的数据变化
   watch: {},
-  methods: {},
+  methods: {
+    clickLeft() {
+      console.log(1)
+    },
+    clickRight() {
+      console.log(2)
+    }
+  },
   // 生命周期-创建之前
   beforeCreate() { },
   // 生命周期-创建完成（可以访问当前this实例）
@@ -44,44 +66,44 @@ export default {
 }
 </script>
 <style scoped lang='scss'>
-.Test {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .shell {
-    width: 180px;
-    height: 180px;
-    // margin: 50px auto;
-    position: relative;
-    border-radius: 35%;
-    box-shadow: 40px 40px 10px 10px rgba(0,0,0,0.3);
-  }
-  .shell::before {
-    content: '';
-    position: absolute;
-    left: 10px;
-    top: 20px;
-    width: 180px;
-    height: 160px;
-    background: #cccccc;
-    border-top-left-radius: 20px 100px;
-    border-top-right-radius: 20px 100px;
-    border-bottom-left-radius: 20px 100px;
-    border-bottom-right-radius: 20px 100px;
-  }
-  .shell::after {
-    content: '';
-    position: absolute;
-    left: 20px;
-    top: 10px;
-    width: 160px;
-    height: 180px;
-    background: #cccccc;
-    border-top-left-radius: 100px 20px;
-    border-top-right-radius: 100px 20px;
-    border-bottom-left-radius: 100px 20px;
-    border-bottom-right-radius: 100px 20px;
-  }
-}
+// .Test {
+//   min-height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   .shell {
+//     width: 180px;
+//     height: 180px;
+//     // margin: 50px auto;
+//     position: relative;
+//     border-radius: 35%;
+//     box-shadow: 40px 40px 10px 10px rgba(0,0,0,0.3);
+//   }
+//   .shell::before {
+//     content: '';
+//     position: absolute;
+//     left: 10px;
+//     top: 20px;
+//     width: 180px;
+//     height: 160px;
+//     background: #cccccc;
+//     border-top-left-radius: 20px 100px;
+//     border-top-right-radius: 20px 100px;
+//     border-bottom-left-radius: 20px 100px;
+//     border-bottom-right-radius: 20px 100px;
+//   }
+//   .shell::after {
+//     content: '';
+//     position: absolute;
+//     left: 20px;
+//     top: 10px;
+//     width: 160px;
+//     height: 180px;
+//     background: #cccccc;
+//     border-top-left-radius: 100px 20px;
+//     border-top-right-radius: 100px 20px;
+//     border-bottom-left-radius: 100px 20px;
+//     border-bottom-right-radius: 100px 20px;
+//   }
+// }
 </style>
